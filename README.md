4008647f6f9927750fc1fe684a66d12c8eb93b0c
Payne <wuzhipeng1289690157@gamil.com>
2021-05-09 02:11:05 +0800

    Upload

---
+++ README.md
@@ -0,0 +1,3 @@
+# Blog Source
+
+Myself Blog Source
--- synchronization.sh
+++ synchronization.sh
@@ -2,4 +2,4 @@
 ## deploy
 hexo clean && hexo generate && hexo deploy
 # synchronization project
-git checkout source && git add -A && git commit -am "Upload" && git push origin  source
+git checkout source && git add -A && git commit -am "Upload" && git push origin source
